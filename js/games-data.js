72b3c1e0ddf8c1037cc9bc4bef9c8240cba311df
IsaacCerejo <isaacerejo2@gmail.com>
2026-01-04 10:26:07 +0000

    feat: data update

--- js/games-data.js
+++ js/games-data.js
@@ -1,23 +1,23 @@
 const games = {
     "game-one": {
         title: "Game One",
-        cover: "../assets/game1.png",
+        cover: "assets/game1.png",
         tags: ["jam", "solo", "programming"],
         itch: "https://yourgame.itch.io/game-one",
         description: "Short experimental game made in 48 hours.",
         contribution: ["Gameplay programming", "Game design","UI & VFX"],
         tools: ["Unity", "C#", "Photoshop"],
-        screenshots: ["../assets/game1.png", "../assets/game1.png", "../assets/game1.png"]
+        screenshots: ["assets/game1.png", "assets/game1.png", "assets/game1.png"]
     },
 
     "game-two": {
         title: "Game Two",
-        cover: "../assets/game2.png",
+        cover: "assets/game2.png",
         tags: ["team", "art", "programming"],
         itch: "#",
         description: "Narrative-focused prototype.",
         contribution: ["Art direction", "Illustration"],
         tools: ["Procreate"],
-        screenshots: ["../assets/game2.png"]
+        screenshots: ["assets/game2.png"]
     }
 };
