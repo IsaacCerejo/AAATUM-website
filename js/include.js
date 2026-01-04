cc9b57e0d1577a1789e778a06c50624ea7a35940
IsaacCerejo <isaacerejo2@gmail.com>
2026-01-04 18:32:35 +0000

    feat: home page improvment

--- js/include.js
+++ js/include.js
@@ -4,7 +4,12 @@ function loadComponent(id, file) {
 
     fetch(file)
         .then(res => res.text())
-        .then(html => container.innerHTML = html)
+        .then(html => {
+            container.innerHTML = html;
+            if (id === "navbar") {
+                initNavbar();
+            }
+        })
         .catch(err => console.error(`Error loading ${file}`, err));
 }
 
@@ -17,9 +22,21 @@ function loadHead(file) {
         .catch(err => console.error("Error loading head:", err));
 }
 
+function initNavbar() {
+    const toggle = document.querySelector(".menu-toggle");
+    const nav = document.querySelector("nav");
+
+    if (!toggle || !nav) return;
+
+    toggle.addEventListener("click", () => {
+        nav.classList.toggle("open");
+    });
+}
 
 document.addEventListener("DOMContentLoaded", () => {
     loadHead("components/head.html");
     loadComponent("navbar", "components/navbar.html");
     loadComponent("footer", "components/footer.html");
 });
+
+
